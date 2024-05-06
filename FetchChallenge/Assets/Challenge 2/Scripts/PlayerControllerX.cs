d15aa6eb674155d5b9cc936eeeb5d5afae343579
Maxwell Hernandez <hernandezm3@roverkids.org>
2024-05-06 13:31:17 -0400

    Added cooldown to spawning dog

--- FetchChallenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ FetchChallenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float cooldown = 1;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && cooldown <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldown = 1;
         }
-    }
+        if (cooldown >= 0)
+            cooldown -= Time.deltaTime;
+    }  
 }
